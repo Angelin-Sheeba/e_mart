96546b082ebb03b2d8d24beaaae90da6c1abfa70
Angelin-Sheeba <angelinsheeba1515@gmail.com>
2024-08-07 07:14:49 -0500

    third commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import Header from './Components/Header'
 import Home from './Components/Home';
+
 
 function App() {
   return (
     <>
-      <Home/>
+      <Header />
+      <Home />
     </>
   );
 }
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,63 +1,106 @@
-import React from "react";
+import React from 'react'
 
-const Navbar = () => {
+const Home = () => {
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-light bg-white py-3 shadow-sm">
-        <div className="container">
-          <a className="navbar-brand fw-bold fs-4" href="#">
-            ANGEL STORE
-          </a>
+      <div
+        id="carouselExampleCaptions"
+        class="carousel slide"
+        data-bs-ride="carousel"
+      >
+        <div class="carousel-indicators">
           <button
-            className="navbar-toggler"
             type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Products
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  About
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Contact
-                </a>
-              </li>
-            </ul>
-            <div className="buttons">
-              <a href="" className="btn btn-outline-dark">
-                <i className="fa fa-sign-in me-1"></i>Login
-              </a>
-              <a href="" className="btn btn-outline-dark ms-2">
-                <i className="fa fa-user-plus me-1"></i>Register
-              </a>
-              <a href="" className="btn btn-outline-dark ms-2">
-                <i className="fa fa-shopping-cart me-1"></i>Cart (0)
-              </a>
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to="0"
+            class="active"
+            aria-current="true"
+            aria-label="Slide 1"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to="1"
+            aria-label="Slide 2"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to="2"
+            aria-label="Slide 3"
+          ></button>
+        </div>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img
+              src="../Assets/Images/ladiesbag.png"
+              class="d-block w-100"
+              alt="LadiesBag"
+              style={{ height: "80vh", objectFit: "cover" }}
+            />
+            <div class="carousel-caption d-none d-md-block bg-white ">
+              <h5 class="text-secondary">
+                Summer Collection Pure Pu Ladies Shoulder Bag
+              </h5>
+              <p class="text-secondary">
+                Nulla vitae elit libero,a augue mollis interdum
+              </p>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img
+              src="../Assets/Images/shirts.png"
+              class="d-block w-100"
+              alt="Shirts"
+              style={{ height: "80vh", objectFit: "cover" }}
+            />
+            <div class="carousel-caption d-none d-md-block bg-white">
+              <h5 class="text-secondary">
+                Summer Collection Young Handsome Guy Smart Suit
+              </h5>
+              <p class="text-secondary">
+                Nulla vitae elit libero, a augue mollis interdum
+              </p>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img
+              src="../Assets/Images/blender.png"
+              class="d-block w-100"
+              alt="Blender"
+              style={{ height: "80vh", objectFit: "cover" }}
+            />
+            <div class="carousel-caption d-none d-md-block bg-white">
+              <h5 class="text-secondary">
+                Summer Collection Blender Fruits Kitchen Space
+              </h5>
+              <p class="text-secondary">
+                Nulla vitae elit libero,a augue mollis interdum
+              </p>
             </div>
           </div>
         </div>
-      </nav>
+        <button
+          class="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide="prev"
+        >
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button
+          class="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide="next"
+        >
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
     </>
   );
-};
+}
 
-export default Navbar;
+export default Home
