488594ac909dbf8fb3a9cc2a5ffcfc3fe30642a1
Angelin-Sheeba <angelinsheeba1515@gmail.com>
2024-08-08 02:06:23 -0500

    sixth commit

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import Skeleton from 'react-loading-skeleton';
 
 
 const Products = () => {
@@ -26,47 +27,93 @@ const Products = () => {
     },[]);
 
     const Loading = () => {
-        return(
-            <>Loading..</>
-        )
+        return (
+          <>
+            <div className="col-md-3">
+              <Skeleton height={350} />
+            </div>
+            <div className="col-md-3">
+              <Skeleton height={350} />
+            </div>
+            <div className="col-md-3">
+              <Skeleton height={350} />
+            </div>
+            <div className="col-md-3">
+              <Skeleton height={350} />
+            </div>
+          </>
+        );
+    }
+
+    const filterProduct = (cat) => {
+        const updatedList = data.filter((x)=>x.category === cat);
+        setFilter(updatedList);
     }
 
     const ShowProducts = () => {
         return (
           <>
             <div className="buttons d-flex justify-content-center mb-5 pb-5">
-              <button className="btn btn-outline-dark me-2">
+              <button
+                className="btn btn-outline-dark me-2"
+                onClick={() => setFilter(data)}
+              >
                 All
               </button>
-              <button className="btn btn-outline-dark me-2">
+              <button
+                className="btn btn-outline-dark me-2"
+                onClick={() => filterProduct("men's clothing")}
+              >
                 Men's Clothing
               </button>
-              <button className="btn btn-outline-dark me-2">
+              <button
+                className="btn btn-outline-dark me-2"
+                onClick={() => filterProduct("women's clothing")}
+              >
                 Women's Clothing
               </button>
-              <button className="btn btn-outline-dark me-2">
+              <button
+                className="btn btn-outline-dark me-2"
+                onClick={() => filterProduct("jewelery")}
+              >
                 Jewelery
               </button>
-              <button className="btn btn-outline-dark me-2">
+              <button
+                className="btn btn-outline-dark me-2"
+                onClick={() => filterProduct("electronics")}
+              >
                 Electronic
               </button>
             </div>
-            {filter.map((product)=>{
-                return(
-                    <>
-                        <div className="col-md-3 mb-4">
-                            <div className="card h-100 text-center p-4" key={product.id}>
-                               <img src={product.image} class="card-img-top" alt={product.title} 
-                               height="250px"/>
-                               <div className="card-body">
-                                   <h5 className="card-title mb-0">{product.title.substring(0,22)}...</h5>
-                                   <p className="card-text lead fw-bold">${product.price}</p>
-                                   <a href="#" className="btn btn-outline-dark">Buy Now</a>
-                                </div>
-                            </div>
-                        </div>
-                    </>
-                )
+            {filter.map((product) => {
+              return (
+                <>
+                  <div className="col-md-3 mb-4">
+                    <div
+                      className="card h-100 text-center p-4"
+                      key={product.id}
+                    >
+                      <img
+                        src={product.image}
+                        class="card-img-top"
+                        alt={product.title}
+                        height="250px"
+                      />
+                      <div className="card-body">
+                        <h5 className="card-title mb-0">
+                          {product.title.substring(0, 22)}...
+                        </h5>
+                        <p className="card-text lead fw-bold">
+                          ${product.price}
+                        </p>
+                        <a href="#" className="btn btn-outline-dark">
+                          Buy Now
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </>
+              );
             })}
           </>
         );
